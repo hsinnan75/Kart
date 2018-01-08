fa5c4eb92e67ae6fd41dec1940d8ff6517c7ff99
hsinnan75 <arith@iis.sinica.edu.tw>
2018-01-08 08:37:05 +0800

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 bwt_t *Refbwt;
 bwaidx_t *RefIdx;
-const char* VersionStr = "2.3.2";
+const char* VersionStr = "2.3.3";
 vector<string> ReadFileNameVec1, ReadFileNameVec2;
 char *RefSequence, *IndexFileName, *OutputFileName;
 int iThreadNum, MaxInsertSize, MaxGaps, MinSeedLength, OutputFileFormat;
